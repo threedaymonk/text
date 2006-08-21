a57f34907c1981e98b37f9b8cdc2b4635b6d5c3c
Paul Battley <pbattley@gmail.com>
2006-08-21 14:56:03 +0000

    Added an option to Metaphone to allow the use of an alternate, 'buggy' ruleset, which is compatible with PHP's implementation.

--- lib/text/metaphone.rb
+++ lib/text/metaphone.rb
@@ -1,27 +1,25 @@
-#
+# 
 # An implementation of the Metaphone phonetic coding system in Ruby.
-#
+# 
 # Metaphone encodes names into a phonetic form such that similar-sounding names
 # have the same or similar Metaphone encodings.
-#
-# As there are multiple implementations of Metaphone, each with their own bugs,
-# I have based this on my reading of the specification.  This implementation
-# has been only lightly tested so far; please report any bugs found to the
-# email address given above.
-#
+# 
+# The original system was described by Lawrence Philips in Computer Language
+# Vol. 7 No. 12, December 1990, pp 39-43.
+# 
+# As there are multiple implementations of Metaphone, each with their own
+# quirks, I have based this on my interpretation of the algorithm specification.
+# Even LP's original BASIC implementation appears to contain bugs (specifically
+# with the handling of CC and MB), when compared to his explanation of the
+# algorithm.
+# 
 # I have also compared this implementation with that found in PHP's standard
-# library.  The present implementation follows the algorithm description,
-# whilst PHP's implementation mimics the behaviour of LP's original BASIC
-# implementation, which appears to contain bugs (specifically with the handling
-# of CC and MB).  The changes required for 100% compatibility are noted in the
-# code, marked with [PHP].  It would be useful to compare the behaviour of
-# other implementations as well.
-#
-# The original system described by Lawrence Philips in Computer Language Vol. 7
-# No. 12, December 1990, pp 39-43:
-#
+# library, which appears to mimic the behaviour of LP's original BASIC
+# implementation. For compatibility, these rules can also be used by passing
+# :buggy=>true to the methods.
+# 
 # Author: Paul Battley (pbattley@gmail.com)
-#
+# 
 module Text
 module Metaphone
 
@@ -31,7 +29,7 @@ module Metaphone
     # Regexp, replacement
     [ /([bcdfhjklmnpqrstvwxyz])\1+/,
                        '\1' ],  # Remove doubled consonants except g.
-                                # [PHP] add c to regexp.
+                                # [PHP] remove c from regexp.
     [ /^ae/,            'E' ],
     [ /^[gkp]n/,        'N' ],
     [ /^wr/,            'R' ],
@@ -60,23 +58,34 @@ module Metaphone
     [ /v/,              'F' ],
     [ /(?!^)[aeiou]+/,  ''  ],
   ]
+  
+  # The rules for the 'buggy' alternate implementation used by PHP etc.
+  #
+  BUGGY_RULES = RULES.dup
+  BUGGY_RULES[0] = [ /([bdfhjklmnpqrstvwxyz])\1+/, '\1' ]
+  BUGGY_RULES[6] = [ /mb/, 'M' ]
 
   # Finds the Metaphone value for a word.  Note that only the letters A-Z are
   # supported, so any language-specific processing should be done beforehand.
   #
-  def metaphone_word(w)
+  # If the :buggy option is set, buggy rules are used.
+  #
+  def metaphone_word(w, options={})
     # Normalise case and remove non-ASCII
     s = w.downcase.gsub(/[^a-z]/, '')
     # Apply the Metaphone rules
-    RULES.each { |rx, rep| s.gsub!(rx, rep) }
+    rules = options[:buggy] ? BUGGY_RULES : RULES
+    rules.each { |rx, rep| s.gsub!(rx, rep) }
     return s.upcase
   end
 
   # Finds the Metaphone values for a string containing multiple words by
-  # calling metaphone_word.
+  # calling metaphone_word for each.
   #
-  def metaphone(str)
-    return str.strip.split(/\s+/).map { |w| metaphone_word(w) }.join(' ')
+  # If the :buggy option is set, buggy rules are used.
+  #
+  def metaphone(str, options={})
+    return str.strip.split(/\s+/).map { |w| metaphone_word(w, options) }.join(' ')
   end
 
   extend self
