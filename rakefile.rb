54b1fa4aab45659e633d6f58ccb1f5c289361bdc
Tim Fletcher <twoggle@gmail.com>
2007-02-20 16:14:54 +0000

    Fixing up the rakefile. Will hopefully work with tinderbox now.

--- rakefile.rb
+++ rakefile.rb
@@ -9,38 +9,36 @@ require 'rake/rdoctask'
 
 gemspec = Gem::Specification.new do |s|
   s.name = 'Text'
-  s.version = '1.1.0'
+  s.version = '1.1.1'
   s.summary ='A collection of text algorithms'
-  s.files = FileList['{lib,test}/**/*.*', '*.txt', 'rakefile.rb']
+  s.files = FileList['{lib,test}/**/*.*', 'README.rdoc', 'rakefile.rb']
   s.require_path = 'lib'
-  s.autorequire = 'text'
-  s.has_rdoc = false
+  s.has_rdoc = true
   s.rubyforge_project = 'text'
   s.homepage = 'http://text.rubyforge.org/'
   s.author = 'Paul Battley, Michael Neumann, Tim Fletcher'
 end
 
-Rake::GemPackageTask.new(gemspec) { |t| t.package_dir = 'gems' }
+Rake::GemPackageTask.new(gemspec) do |pkg|
+  pkg.need_tar_gz = true
+end
 
-Rake::PackageTask.new('Text', '1.1.0') do |p|
-  p.need_tar_gz = true
-  p.package_files.include('lib/**/*.rb')
+Rake::PackageTask.new(gemspec.name, gemspec.version) do |pkg|
+  pkg.need_tar_gz = true
+  pkg.package_files.include gemspec.files
 end
 
 Rake::TestTask.new do |t|
-  t.test_files = FileList['test/*_test.rb']
   t.verbose = false
 end
 
 Rcov::RcovTask.new do |t|
-  t.test_files = FileList['test/*_test.rb']
-  t.output_dir = 'coverage'
   t.rcov_opts = []
 end
 
 Rake::RDocTask.new do |t|
-  t.main = "README.rdoc"
-  t.rdoc_files.include("README.rdoc", "lib/**/*.rb")
+  t.main = 'README.rdoc'
+  t.rdoc_files.include 'README.rdoc', 'lib/**/*.rb'
 end
 
 task :default => :test
