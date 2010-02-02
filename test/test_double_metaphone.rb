c0fa116b314d472b146e322e2a6ea2a2723d00c5
Hampton Catlin <hcatlin@gmail.com>
2010-02-02 13:25:09 +0000

    Removed support for FasterCSV as this is the standard in 1.9 and the messy (ugly, ugly, ugly) code required to give FasterCSV support for just the tests wasn't worth it in the least.
    It makes the test slightly faster, but makes the
    code non-1.9 compatible and extremely weird to read.

--- test/test_double_metaphone.rb
+++ test/test_double_metaphone.rb
@@ -1,20 +1,13 @@
 require File.join(File.dirname(__FILE__), 'preamble')
 
-begin
-  require 'rubygems' rescue nil
-  require 'fastercsv'
-  METHOD = [ FasterCSV, :foreach, { :col_sep => ', ' } ]
-rescue LoadError
-  require 'csv'
-  METHOD = [ CSV, :open, 'r', ', ' ]
-end
+require 'csv'
 
 class DoubleMetaphoneTest < Test::Unit::TestCase
 
   def test_cases
-    METHOD.shift.send(METHOD.shift, File.rel('data', 'double_metaphone.csv'), *METHOD) do |row|
+    CSV.open(File.rel('data', 'double_metaphone.csv'), 'r').to_a.each do |row|
       primary, secondary = Text::Metaphone.double_metaphone(row[0])
-
+      
       assert_equal row[1], primary
       assert_equal row[2], secondary.nil?? primary : secondary
     end
