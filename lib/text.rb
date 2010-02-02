4b95ce6ff5093b6bb95ffd4969436bcb9135958a
Hampton Catlin <hcatlin@gmail.com>
2010-02-02 13:23:06 +0000

    Created a general way to determine the encoding for a string. Works in both 1.8 and 1.9. There is far too many $KCODE strings spread around with repeated logic.

--- lib/text.rb
+++ lib/text.rb
@@ -5,3 +5,13 @@ require 'text/metaphone'
 require 'text/porter_stemming'
 require 'text/soundex'
 require 'text/version'
+
+module Text
+  def self.encoding_of(string)
+    if defined?(Encoding) 
+      string.encoding.to_s 
+    else 
+      $KCODE
+    end
+  end
+end
