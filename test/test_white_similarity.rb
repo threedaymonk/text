17f57005a5c042d5fa24d0114dadc6cbbc7647fd
Paul Battley <pbattley@gmail.com>
2011-11-21 20:57:53 +0000

    Test with cached implementation.

--- test/test_white_similarity.rb
+++ test/test_white_similarity.rb
@@ -2,6 +2,7 @@ require File.join(File.dirname(__FILE__), 'preamble')
 require "text/white_similarity"
 
 class WhiteSimilarityTest < Test::Unit::TestCase
+
   def test_similarity
     word = "Healed"
 
@@ -11,5 +12,18 @@ class WhiteSimilarityTest < Test::Unit::TestCase
     assert_in_delta 0.40, Text::WhiteSimilarity.compare_strings(word, "Herded"),  0.01
     assert_in_delta 0.25, Text::WhiteSimilarity.compare_strings(word, "Help"),    0.01
     assert_in_delta 0.0,  Text::WhiteSimilarity.compare_strings(word, "Sold"),    0.01
+  end
+
+  def test_similarity_with_caching
+    word = "Healed"
+
+    similarity = Text::WhiteSimilarity.new
+
+    assert_in_delta 0.8,  similarity.compare_strings(word, "Sealed"),  0.01
+    assert_in_delta 0.55, similarity.compare_strings(word, "Healthy"), 0.01
+    assert_in_delta 0.44, similarity.compare_strings(word, "Heard"),   0.01
+    assert_in_delta 0.40, similarity.compare_strings(word, "Herded"),  0.01
+    assert_in_delta 0.25, similarity.compare_strings(word, "Help"),    0.01
+    assert_in_delta 0.0,  similarity.compare_strings(word, "Sold"),    0.01
   end
 end
