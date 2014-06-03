21425450c93fa4075f345c206acdb5c35f2b302e
Paul Battley <pbattley@gmail.com>
2014-06-03 13:01:21 +0100

    Move Soundex test data to file

--- test/soundex_test.rb
+++ test/soundex_test.rb
@@ -5,22 +5,7 @@ require 'yaml'
 class SoundexTest < Test::Unit::TestCase
 
   def test_cases
-    YAML.load(%{
-
-      Euler: E460
-      Ellery: E460
-      Gauss: G200
-      Ghosh: G200
-      Hilbert: H416
-      Heilbronn: H416
-      Knuth: K530
-      Kant: K530
-      Lloyd: L300
-      Ladd: L300
-      Lukasiewicz: L222
-      Lissajous: L222
-
-    }).each do |input, expected_output|
+    YAML.load(data_file('soundex.txt')).each do |input, expected_output|
       assert_equal expected_output, Text::Soundex.soundex(input)
     end
   end
